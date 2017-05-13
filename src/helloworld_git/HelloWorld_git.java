278ff0d8db68556a32c55880ab7cde0a0378433a
jonasschwartz <jonasschwartz@localhost>
2017-05-13 16:22:16 +0200

    änderung hinzugefügt

--- src/helloworld_git/HelloWorld_git.java
+++ src/helloworld_git/HelloWorld_git.java
@@ -14,8 +14,10 @@ public class HelloWorld_git {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
+        System.out.println("Test");
     }
     
 }
